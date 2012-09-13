2d7f81d0f635a2f84747e2877d31b005184dd2b0
Patrick Connolly <patrick.c.connolly@gmail.com>
2012-09-13 01:34:51 -0400

    Using 'default_action' syntax in LWRP.

--- resources/profile.rb
+++ resources/profile.rb
@@ -1,10 +1,5 @@
+default_action :create
 actions :create
 
 attribute :name, :kind_of => String, :name_attribute => true
 attribute :user, :kind_of => String
-
-
-def initialize(name, run_context=nil)
-  super
-  @action = :create
-end
