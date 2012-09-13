9a422ea10564bfbc382aa8e7eb90b7294b8ef351
Patrick Connolly <patrick.c.connolly@gmail.com>
2012-09-12 22:15:05 -0400

    Added user resource attr. libraries/user.rb from pivotal_workstation no longer needed.

--- providers/profile.rb
+++ providers/profile.rb
@@ -1,19 +1,22 @@
+user = new_resource.user
+home = system("echo ~#{user}").chomp
+
 ::BASH_INCLUDES_DIRNAME = ".bash_profile.d"
-::BASH_INCLUDES_SUBDIR = ::File.expand_path("#{WS_HOME}/#{BASH_INCLUDES_DIRNAME}")
+::BASH_INCLUDES_SUBDIR = ::File.expand_path("#{home}/#{BASH_INCLUDES_DIRNAME}")
 
 action :create do
   directory BASH_INCLUDES_SUBDIR do
-    owner WS_USER
+    owner user
   end
 
-  template "#{WS_HOME}/.bash_profile" do
+  template "#{home}/.bash_profile" do
     source "bash_profile.erb"
-    owner WS_USER
+    owner user
   end
 
   template "#{BASH_INCLUDES_SUBDIR}/#{new_resource.name}.sh" do
     source "bash_profile-#{new_resource.name}.sh.erb"
-    owner WS_USER
+    owner user
     mode "0755"
   end
 end
--- resources/profile.rb
+++ resources/profile.rb
@@ -1,6 +1,7 @@
 actions :create
 
 attribute :name, :kind_of => String, :name_attribute => true
+attribute :user, :kind_of => String
 
 
 def initialize(name, run_context=nil)
