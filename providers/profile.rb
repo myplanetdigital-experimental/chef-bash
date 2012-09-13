17acab68440c7094c97bd159c821203ec544ec21
Patrick Connolly <patrick.c.connolly@gmail.com>
2012-09-13 01:37:18 -0400

    Got bash_profile LWRP working.

--- providers/profile.rb
+++ providers/profile.rb
@@ -1,20 +1,22 @@
-user = new_resource.user
-home = system("echo ~#{user}").chomp
-
-::BASH_INCLUDES_DIRNAME = ".bash_profile.d"
-::BASH_INCLUDES_SUBDIR = ::File.expand_path("#{home}/#{BASH_INCLUDES_DIRNAME}")
-
 action :create do
-  directory BASH_INCLUDES_SUBDIR do
+  user = new_resource.user
+  home = %x[echo ~#{user}].chomp
+  cookbook = new_resource.cookbook || new_resource.cookbook_name
+  ::BASH_PROFILE_DIRNAME = ".bash_profile.d"
+  ::BASH_PROFILE_SUBDIR = ::File.expand_path("#{home}/#{BASH_PROFILE_DIRNAME}")
+
+  directory BASH_PROFILE_SUBDIR do
     owner user
   end
 
   template "#{home}/.bash_profile" do
+    cookbook "bash"
     source "bash_profile.erb"
     owner user
   end
 
-  template "#{BASH_INCLUDES_SUBDIR}/#{new_resource.name}.sh" do
+  template "#{BASH_PROFILE_SUBDIR}/#{new_resource.name}.sh" do
+    cookbook cookbook
     source "bash_profile-#{new_resource.name}.sh.erb"
     owner user
     mode "0755"
--- resources/profile.rb
+++ resources/profile.rb
@@ -3,3 +3,4 @@ actions :create
 
 attribute :name, :kind_of => String, :name_attribute => true
 attribute :user, :kind_of => String
+attribute :cookbook, :kind_of => String, :default => nil
